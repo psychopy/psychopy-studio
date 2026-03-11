f2cb4898f063577dd0a588eba767a8715f45b7b5
TEParsons <todd.e.parsons@gmail.com>
2026-03-11 10:43:05 +0000

    FF: expires_in is in seconds, but needs to be in milliseconds

--- src/lib/pavlovia/pavlovia.svelte.js
+++ src/lib/pavlovia/pavlovia.svelte.js
@@ -115,7 +115,7 @@ async function refreshToken(username) {
     // queue up next refresh in case token expires this session
     setTimeout(
         evt => refreshToken(username), 
-        data.expires_in
+        data.expires_in * 1000
     )
 
     return data;
