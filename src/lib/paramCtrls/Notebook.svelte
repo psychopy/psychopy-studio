0ed22951e0c436ed72c32a5ac45674b766df0d4e
Todd Parsons <todd.e.parsons@gmail.com>
2026-06-01 18:13:06 +0100

    FF: Make params notebook full width of dialog

--- src/lib/paramCtrls/Notebook.svelte
+++ src/lib/paramCtrls/Notebook.svelte
@@ -196,7 +196,7 @@
     .params-container {
         padding: 1rem;
         height: 100%;
-        max-width: 65rem;
+        width: 100%;
         box-sizing: border-box;
         display: flex;
         flex-direction: column;
